e7413f3c9aab501600c86cb1f09634f4a8a1806a
Atsz7 <angeltsz74@gmail.com>
2026-08-25 11:46:32 -0600

    Removing turbine for the moment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,7 +81,6 @@ dependencies {
     testImplementation(libs.junit)
     testImplementation(libs.mockk)
     testImplementation(libs.kotlinx.coroutines.test)
-    testImplementation(libs.turbine)
     testImplementation(libs.paging.testing)
 
     // Instrumentation tests
